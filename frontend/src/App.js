84ff48cf5b39448bf915a7c9e71c8a1329614069
NicoleTomaszewski <tomaszewski.nicole@gmail.com>
2022-02-19 10:57:15 -0500

    playing around with React

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import UserProfile from '../../purcleapp/models.py';
 
 function App() {
   return (
@@ -17,6 +18,9 @@ function App() {
         >
           Learn React
         </a>
+        <p>
+          {UserProfile.objects.all()}
+        </p>
       </header>
     </div>
   );
