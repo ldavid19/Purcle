55581ad58d8410923ed63cf3d2626939475e212e
David Li <li3306@purdue.edu>
2022-02-22 17:39:19 -0500

    Added Login page (Login.js). Connected React to Django backend. Deleted package-lock.json from parent directory.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react';
 import Home from "../src/components/Home/Home.js";
 import SignUp from './components/Signup/SignUp';
+import Login from './components/Login/Login';
 import NavigationBar from './components/Navbar/NavigationBar';
 
 
