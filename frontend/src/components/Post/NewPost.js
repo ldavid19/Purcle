ce1c1b9ba407ce5288e87bb636d9f6aa5276df7e
NicoleTomaszewski <tomaszewski.nicole@gmail.com>
2022-02-22 16:47:33 -0500

    added error messages for Create Post UI

--- frontend/src/components/Post/NewPost.js
+++ frontend/src/components/Post/NewPost.js
@@ -1,12 +1,47 @@
 import { useState } from "react";
 import React from 'react';
 import { Button, Modal } from "react-bootstrap";
+import Select from 'react-select';
+
+function errorMessage(title, type, text, image) {
+    let message = "";
+    if (type.localeCompare("Image") !== 0 && type.localeCompare("Text") !== 0) {
+        message = "A post type needs to be selected.\n";
+    }
+    if (title.length === 0) {
+        message = message + "Your post needs a title.\n";
+    }
+    if (type.localeCompare("Text") === 0 && text.length === 0) {
+        message = message + "Please insert text.\n";
+    }
+    if (type.localeCompare("Image") === 0 && image.length === 0) {
+        message = message + "Please insert image.\n";
+    }
+    return message;
+}
 
 function NewPost() {
+
     const [show, setShow] = useState(false);
+
+    const handleSubmit = () =>{
+        setError(errorMessage(title, type, text, image));
+    }
+
+    const [error, setError] = React.useState("");
   
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+    const handleClose = () => {
+        setShow(false);
+        setTitle("");
+        setImage("");
+        setText("");
+        setType("");
+        setError("");
+        setChecked(false);
+    }
+    const handleShow = () => {
+        setShow(true);
+    };
 
     const [checked, setChecked] = React.useState(false);
     const handleAnonChange = () => {
@@ -14,21 +49,31 @@ function NewPost() {
     };
 
     const [title, setTitle] = React.useState("");
-    const handleTitleChange = ev => setTitle(ev.target.value);
+    const handleTitleChange = ev => {
+        setTitle(ev.target.value);
+    };
 
-    const [photo, setPhoto] = React.useState("");
-    const handlePhotoChange = ev => setPhoto(ev.target.value);
+    const [image, setImage] = React.useState("");
+    const handleImageChange = ev => {
+        setImage(ev.target.value);
+    };
 
     const [text, setText] = React.useState("");
-    const handleTextChange = ev => setText(ev.target.value);
+    const handleTextChange = ev => {
+        setText(ev.target.value);
+    };
 
     const [type, setType] = React.useState("");
-    const handleTypeChange = ev => setType(ev.target.value);
-  
+    const handleTypeChange = ev => {
+        setType(ev.target.value);
+        setImage("");
+        setText("");
+    };
+
     return (
         <>
             <Button variant="primary" onClick={handleShow}>
-                Write new post!
+                Create new post!
             </Button>
     
             <Modal show={show} onHide={handleClose}>
@@ -39,7 +84,7 @@ function NewPost() {
                 <select onChange={handleTypeChange}>                        
                     <option type="default" selected disabled hidden>Choose post type</option>
                     <option type="text">Text</option>
-                    <option type="photo">Photo</option>
+                    <option type="image">Image</option>
                 </select>
                 {' '}
                 <label>
@@ -54,7 +99,6 @@ function NewPost() {
                     placeholder=" Title"
                     value={title || ""}
                     onChange={handleTitleChange}
-                    isInvalid={!title}
                     style={{width: "465px"}}
                     maxlength="100"
                     rows={1}
@@ -62,14 +106,13 @@ function NewPost() {
                 />
                 <p></p>
                 { 
-                    type.localeCompare("Photo") === 0 || type.localeCompare("Text") === 0 ?
-                        type.localeCompare("Photo") !== 0 ?
+                    type.localeCompare("Image") === 0 || type.localeCompare("Text") === 0 ?
+                        type.localeCompare("Image") !== 0 ?
                             <textarea
                                 name="text"
                                 placeholder=" Insert text here"
                                 value={text || ""}
-                                onChange={handleTextChange}
-                                isInvalid={!text}                            
+                                onChange={handleTextChange}                       
                                 style={{width: "465px"}}
                                 maxlength="500"
                                 rows={5}
@@ -77,11 +120,10 @@ function NewPost() {
                             />
                         :
                             <textarea
-                                name="photo"
-                                placeholder=" Photo"
-                                value={photo || ""}
-                                onChange={handlePhotoChange}
-                                isInvalid={!photo}
+                                name="image"
+                                placeholder=" Image"
+                                value={image || ""}
+                                onChange={handleImageChange}
                                 style={{width: "465px"}}
                                 maxlength="100"
                                 rows={1}
@@ -95,8 +137,8 @@ function NewPost() {
                 {/*<Button variant="secondary" onClick={handleClose}>
                 Cancel
                 </Button> */}
-                <p>{checked ? ' ' : 'ERROR'}</p>
-                <Button variant="primary" onClick={handleClose}>
+                <p>{error}</p>
+                <Button variant="primary" onClick={handleSubmit}>
                 Post
                 </Button>
             </Modal.Footer>
