a5c81b34c22eb5b8b4284ec346f15399c744c14c
NicoleTomaszewski <tomaszewski.nicole@gmail.com>
2022-02-22 02:00:06 -0500

    added most of the Create Post UI

--- frontend/src/components/Post/NewPost.js
+++ frontend/src/components/Post/NewPost.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import React from 'react';
 import { Button, Modal } from "react-bootstrap";
-import {View, StyleSheet, Text} from 'react-native';
 
 function NewPost() {
     const [show, setShow] = useState(false);
@@ -10,9 +9,21 @@ function NewPost() {
     const handleShow = () => setShow(true);
 
     const [checked, setChecked] = React.useState(false);
-    const handleChange = () => {
+    const handleAnonChange = () => {
         setChecked(!checked);
     };
+
+    const [title, setTitle] = React.useState("");
+    const handleTitleChange = ev => setTitle(ev.target.value);
+
+    const [photo, setPhoto] = React.useState("");
+    const handlePhotoChange = ev => setPhoto(ev.target.value);
+
+    const [text, setText] = React.useState("");
+    const handleTextChange = ev => setText(ev.target.value);
+
+    const [type, setType] = React.useState("");
+    const handleTypeChange = ev => setType(ev.target.value);
   
     return (
         <>
@@ -22,25 +33,71 @@ function NewPost() {
     
             <Modal show={show} onHide={handleClose}>
             <Modal.Header closeButton>
-                <Modal.Title>New post</Modal.Title>
+                <Modal.Title>New Post</Modal.Title>
             </Modal.Header>
             <Modal.Body>
+                <select onChange={handleTypeChange}>                        
+                    <option type="default" selected disabled hidden>Choose post type</option>
+                    <option type="text">Text</option>
+                    <option type="photo">Photo</option>
+                </select>
+                {' '}
                 <label>
                     Anonymous{' '}
                     <input type="checkbox" 
                     checked={checked}
-                    onChange={handleChange}/>
+                    onChange={handleAnonChange}/>
                 </label>
+                <p></p>
+                <textarea
+                    name="title"
+                    placeholder=" Title"
+                    value={title || ""}
+                    onChange={handleTitleChange}
+                    isInvalid={!title}
+                    style={{width: "465px"}}
+                    maxlength="100"
+                    rows={1}
+                    cols={5}
+                />
+                <p></p>
+                { 
+                    type.localeCompare("Photo") === 0 || type.localeCompare("Text") === 0 ?
+                        type.localeCompare("Photo") !== 0 ?
+                            <textarea
+                                name="text"
+                                placeholder=" Insert text here"
+                                value={text || ""}
+                                onChange={handleTextChange}
+                                isInvalid={!text}                            
+                                style={{width: "465px"}}
+                                maxlength="500"
+                                rows={5}
+                                cols={5}
+                            />
+                        :
+                            <textarea
+                                name="photo"
+                                placeholder=" Photo"
+                                value={photo || ""}
+                                onChange={handlePhotoChange}
+                                isInvalid={!photo}
+                                style={{width: "465px"}}
+                                maxlength="100"
+                                rows={1}
+                                cols={5}
+                            />
+                    :
+                        <p></p>
+                }
             </Modal.Body>
             <Modal.Footer>
                 {/*<Button variant="secondary" onClick={handleClose}>
                 Cancel
-    </Button> */}
-                <Text style={[{textAlign:'left'}]}>
-                    <p>{checked ? ' ' : checked.toString()}</p>
-                </Text>
+                </Button> */}
+                <p>{checked ? ' ' : 'ERROR'}</p>
                 <Button variant="primary" onClick={handleClose}>
-                Submit
+                Post
                 </Button>
             </Modal.Footer>
             </Modal>
