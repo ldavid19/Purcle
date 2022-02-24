2a8af25101da857ba23e00564a5e5144bd313aa4
David Li <li3306@purdue.edu>
2022-02-23 23:06:46 -0500

    new react build 2/23 11:06pm

---
+++ frontend/build/precache-manifest.a4ab077aaf05be09111c87051c4fff5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ba24eeb2c646c8a7da39dceac95170c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aba72451f135d778b7cf",
+    "url": "/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "ea8362490af401dbc5df",
+    "url": "/static/css/main.ecfd2eea.chunk.css"
+  },
+  {
+    "revision": "aba72451f135d778b7cf",
+    "url": "/static/js/2.90f550ba.chunk.js"
+  },
+  {
+    "revision": "472c8783edcf2167338f",
+    "url": "/static/js/3.32d57b79.chunk.js"
+  },
+  {
+    "revision": "ea8362490af401dbc5df",
+    "url": "/static/js/main.f32c4597.chunk.js"
+  },
+  {
+    "revision": "b8db872f1c1231036589",
+    "url": "/static/js/runtime~main.4548667a.js"
+  }
+]);
